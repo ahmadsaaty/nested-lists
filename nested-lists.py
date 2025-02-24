94df03aa186714a94fdea36c13859664bd0ba916
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-02-24 17:39:59 +0200

    added for loop and append

---
+++ nested-lists.py
@@ -0,0 +1,10 @@
+lists= []
+listn= []
+for _ in range(3):
+    name = input("enter name: ")
+    score = float(input("enter grade: "))
+    listn = [name, score]
+    
+    print(listn)
+lists.append(listn)
+print(lists)
